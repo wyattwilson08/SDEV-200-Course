47e6275d954e4df820b5b697e0f66db8f5487450
Wyatt Wilson <wyattwilson08@gmail.com>
2026-08-31 15:12:48 -0400

    Add files via upload

---
+++ Chapter 1/ArimetheticDemo.java
@@ -0,0 +1,28 @@
+//Wyatt Wilson
+// P.71 AUG. 31
+
+import java.util.Scanner;
+
+public class ArimetheticDemo 
+{
+    public static void main(string[] args)
+    {
+        int firstNumber;
+        int secondNumber;
+        int sum;
+        int difference;
+        int average;
+        Scanner input = Scanner(System.in);
+        System.out.print(s:"Please intput an integer >>");
+        firstNumber = input.nextInt();
+        System.out.print(s:"Please intput another integer >>");
+        secondNumberNumber = input.nextInt();
+        sum = firstNumber + secondNumber;
+        difference = firstNumber - secondNumber;
+        average = sum / 2;
+        System.out.printIn(firstNumber + "+" + secondNumber + "Is" + sum )
+        System.out.printIn(firstNumber + "-" + secondNumber + "Is" + difference)
+        System.out.printIn("The Average of" firstNumber + "and" + secondNumber + "Is" + average )
+
+    }
+}
