7c99581f78459a8b959e5a67f15ee02cb5049708
Wyatt Wilson <wyattwilson08@gmail.com>
2026-09-14 14:01:40 -0400

    Add files via upload

---
+++ CH3/ParadiseInfo.java
@@ -0,0 +1,12 @@
+// Wyatt Wilson
+// p89 
+public class ParadiseInfo {
+    public static void main(String[] args) {
+        displayInfo();
+    }
+
+    public static void displayInfo() {
+        System.out.println("Paradise Day Spa wants to pamper you.");
+        System.out.println("We will make you look good.");
+    }
+}
