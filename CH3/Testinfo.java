b513edd7a25eccb40c71344f67280b820a4d3b1e
Wyatt Wilson <wyattwilson08@gmail.com>
2026-09-14 14:07:08 -0400

    Add files via upload

---
+++ CH3/Testinfo.java
@@ -0,0 +1,9 @@
+//Wyatt Wilson
+//p90
+
+public class Testinfo {
+    public static void main(String[] args) {
+        System.out.println("Calling method from another class:");
+        ParadiseInfo.displayInfo();
+    }
+}
